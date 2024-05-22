ea89221c99829de9dd5915a0662bd8d4ad380662
Caioalves14 <caio.alvesrex@gmail.com>
2024-05-22 10:34:42 -0300

    Adicionando a tela e o método de criar post

--- blog/src/routes/Home.jsx
+++ blog/src/routes/Home.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import blogFetch from '../axios/config'
+
 import { useState, useEffect } from 'react'
 import {Link} from "react-router-dom"
-import axios from "axios"
 
 import "./Home.css"
 
@@ -13,10 +13,13 @@ const Home = () => {
     
     try {
 
-      const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      const response = await blogFetch.get(
+        "/posts"
+      );
       
       const data = response.data ;
-      console.log(data)
+      
+      setPosts(data);
     } catch (error) {
       console.log(error)
     }
@@ -26,7 +29,18 @@ const Home = () => {
     getPosts()
   }, [])
   return (
-    <div>Home</div>
+    <div className='home'>
+      <h1>Últimos post</h1>
+      {posts.length === 0 ? <p>Carregando...</p> : (
+        posts.map((post) => (
+          <div className='post' key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/posts/${post.id}`} className='btn'>Ler mais</Link>
+          </div>
+        ))
+      )}
+    </div>
   )
 }
 
